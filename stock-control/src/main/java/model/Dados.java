7423a5f08c8a2582ad92d6e8a4b1182e77430b48
Lufyo <francalufyo@gmail.com>
2023-11-28 21:57:23 -0300

    Criação das JFrames (views) no app

--- stock-control/src/main/java/model/Dados.java
+++ stock-control/src/main/java/model/Dados.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Dados {
-
+    protected String username, password;
+    
+    public void setUsername(String user){
+        this.username = user;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public void setPassword(String pass){
+        this.password = pass;
+    }
+    public String getPassword(){
+        return password;
+    }
 }
---
+++ stock-control/src/main/java/view/Home.java
@@ -0,0 +1,66 @@
+package view;
+
+public class Home extends javax.swing.JFrame {
+
+    public Home() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenu1.setText("Cadastrar Produtos");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Teste");
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Teste2");
+        jMenuBar1.add(jMenu3);
+
+        jMenu4.setText("Teste3");
+        jMenuBar1.add(jMenu4);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 277, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Home().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenuBar jMenuBar1;
+    // End of variables declaration//GEN-END:variables
+}
